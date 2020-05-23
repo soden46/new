18eb8f18d69e0dbe096786f617bcabc645a7c880
soden46 <65828433+soden46@users.noreply.github.com>
2020-05-24 02:44:47 +0700

    Add files via upload

---
+++ safe.js
@@ -0,0 +1,26 @@
+ User-agent: *
+Disallow: /*?*
+Disallow: /search
+Allow: / 
+
+isi artikel bagian atas . . .<br/>
+-content-
+<div dir="ltr" style="text-align: left;" trbidi="on">
+<center>
+<h2 id="daplong" style="color: daplong;">
+Please Wait...</h2>
+<script src="https://yourjavascript.com/31521315663/401xd-download.js" type="text/javascript"></script> <script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://www.jogjaweb.xyz/2020/05/safelink-page.html?url=", "");
+document.write('<button id="download" class="Visit_Link" onclick="changeLink();" style="display: none;text-align:justify;margin-bottom:2px;">Visit Link</button>')
+</script></center>
+</div><br/>
+-content-<br/>
+isi artikel bagian bawah. . .
+
+<script type="text/javascript" src="http://yourjavascript.com/21103006175/javascript.js"></script>
+<script type="text/javascript">
+protected_links = "facebook.com";
+auto_safelink();
+</script>
